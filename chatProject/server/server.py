d45c65859e4cb3c228336d9be90780835b624e6b
jsheng0722 <57469451+jsheng0722@users.noreply.github.com>
2020-04-07 15:38:24 -0700

    Update Server.py
    This may not work it should modify something till work

--- chatProject/server/server.py
+++ chatProject/server/server.py
@@ -1,6 +1,40 @@
-from socket import *
+import socket
 import json
+from threading import Thread
+
+class Client(Thread):
+    def __init__(self, ip, port):
+        self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.client.connect((ip,port))
+        while True:
+            a = input()
+            self.client.send(a.encode("utf-8"))
+            data = self.client.recv(1024)
+            print(data.decode("uft8"))
 
 class Server:
-    def __init__(self):
-        pass
+    def __init__(self, ip, port):
+        self.server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.server.bind((ip,port))
+        self.server.listen(5)
+        # connection, address = self.server.accept()
+        while True:
+            connection, address = self.server.accept()
+            client_thread = Thread(target=self.handle_sock, args=(connection,address))
+            data = connection.recv(1024)
+            print(data.decode("utf-8"))
+            a = input("")
+            print(data.decode("uft8"))
+            client_thread.start()
+
+    def handle_sock(self, connection, address):
+        self.connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        while True:
+            data = connection.recv(1024)
+            b = data.decode("utf-8")
+            print(b)
+            a = input("")
+            self.connection.send(a.encode("utf8"))
+
+if __name__ == '__main__':
+    client = Client('127,0,0,1',10086).start()
