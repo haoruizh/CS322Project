8fd62e16ffa8bc9a5a868cef748051aa9dbe1ffa
hzhang <hzhang@eecs.wsu.edu>
2020-04-20 21:18:26 -0700

    add basic property for userIdentify class

--- chatProject/server/Friendlist.py
+++ chatProject/server/Friendlist.py
@@ -3,4 +3,24 @@ import json
 
 class friendlist:
     def __init__(self):
-        pass
+        self.ContactList = []
+    def View(self):
+        for i in self.ContactList:
+            print(i)
+    def Add(self, Name):
+        #if searched user not in the list
+        if Name not in self.ContactList:
+            #If try to add own name, throw an error
+            self.ContactList += (Name, None)
+        else:
+            print("Searched user already in the list!")
+    def Remove(self, Name):
+        if Name in self.ContactList:
+            self.ContactList.remove(Name)
+        else:
+            print("Searched user not in the list!")
+    def Find(self, Name):
+        if Name in self.ContactList:
+            return self.ContactList[Name]
+        else:
+            print("Searched user not in list!")
--- chatProject/server/UserIdentify.py
+++ chatProject/server/UserIdentify.py
@@ -2,5 +2,14 @@ from socket import *
 import json
 
 class UserIdentify:
-    def __init__(self):
-        pass
+    def __init__(self, userName, password):
+        self.Username = userName
+        self.Password = password
+    def GetName(self):
+        return self.Username
+    def ChangePassword(self, oldPassword, newPassword):
+        if self.Password == oldPassword:
+            self.Password = newPassword
+    def GetPassword(self):
+        return self.Password
+    
