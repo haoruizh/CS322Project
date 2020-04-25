eccd2f0cf53e41817203580d9132c9f8ad211643
hzhang <hzhang@eecs.wsu.edu>
2020-04-25 16:59:13 -0700

    Add comment

--- chatProject/server/UserIdentify.py
+++ chatProject/server/UserIdentify.py
@@ -5,13 +5,21 @@ class UserIdentify:
     def __init__(self, userName, password):
         self.Username = userName
         self.Password = password
+
+    # get the username of this user
     def GetName(self):
         return self.Username
+
+    # change the password
     def ChangePassword(self, oldPassword, newPassword):
         if self.Password == oldPassword:
             self.Password = newPassword
+
+    # get the password 
     def GetPassword(self):
         return self.Password
+        
+    # check if given username and password match
     def VerifyCheck(self, userName, password):
         if userName != self.Username or password != self.Password:
             return False
