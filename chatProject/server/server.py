7b54d6f0f447b4d69f2c772f7870f28113ddc65f
MSJPurF <44460320+dgmpurf@users.noreply.github.com>
2020-04-29 00:25:31 -0700

    Add files via upload

--- chatProject/server/server.py
+++ chatProject/server/server.py
@@ -1,64 +1,140 @@
 import socket
-import threading
-import sys
+import select
+from UserIdentify_dic import UserIdentify_dic
 
-class Server:
-    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-    # connections = []
-    def __init__(self, ip, port):
-        self.sock.bind((ip, port))
-        self.sock.listen()
+HEADER_LENGTH = 10
 
-    # send msg, if no msg break, else send
-    def handler(self, cnt, address):
-        while True:
-            msg = input(">>")
-            msg = msg.encode()
-            cnt.send(msg)
-            data = cnt.recv(1024)
-            data = data.encode()
-            # for connection in self.connections:
-            #     connection.send(data)
-            if not data:
-                print(str(address[0]) + ':' + str(address[1], "disconnected"))
-                self.connections.remove(cnt)
-                cnt.close()
-                break
+IP = "127.0.0.1"
+PORT = 1234
 
-    # connection and address should be accept
-    def run(self):
-        while True:
-            cnt,address = self.sock.accept()
-            cThread = threading.Thread(target=self.handler,args=(cnt,address))
-            cThread.daemon = True
-            cThread.start()
-            self.connections.append(cnt)
-            print(str(address[0]) + ':' + str(address[1], "connected"))
+# Create a socket
+# socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
+# socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-class Client:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    def sendMsg(self):
-        while True:
-            self.sock.send(bytes(input(""),'utf-8'))
+# SO_ - socket option
+# SOL_ - socket option level
+# Sets REUSEADDR (as a socket option) to 1 on socket
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    def __init__(self,address,port):
-        self.sock.connect((address, port))
-        iThread = threading.Thread(target=self.sendMsg)
-        iThread.daemon = True
-        iThread.start()
-        while True:
-            data = self.sock.recv(1024)
-            data = data.decode()
-            msg = input(str(">>"))
-            msg = msg.encode()
-            if not data:
-                break
-            print(data)
+# Bind, so server informs operating system that it's going to use given IP and port
+# For a server using 0.0.0.0 means to listen on all available interfaces, useful to connect locally to 127.0.0.1 and remotely to LAN interface IP
+server_socket.bind((IP, PORT))
 
-if __name__ == '__main__':
-    if (len(sys.argv)>1):
-        client = Client(sys.argv[1], 1000)
-    else:
-        server = Server('192.168.1.104', 1000)
-        server.run
+# This makes server listen to new connections
+server_socket.listen()
+
+# List of sockets for select.select()
+sockets_list = [server_socket]
+
+# List of connected clients - socket as a key, user header and name as data # password
+clients = {}
+usern = {}
+userp = {}
+userdic = UserIdentify_dic()
+
+
+print(f'Listening for connections on {IP}:{PORT}...')
+
+# Handles message receiving
+def receive_message(client_socket):
+
+    try:
+
+        # Receive our "header" containing message length, it's size is defined and constant
+        message_header = client_socket.recv(HEADER_LENGTH)
+
+        # If we received no data, client gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+        if not len(message_header):
+            return False
+
+        # Convert header to int value
+        message_length = int(message_header.decode('utf-8').strip())
+
+        # Return an object of message header and message data
+        return {'header': message_header, 'data': client_socket.recv(message_length)}
+
+    except:
+
+        # socket.close() also invokes socket.shutdown(socket.SHUT_RDWR) what sends information about closing the socket (shutdown read/write)
+        # and that's also a cause when we receive an empty message
+        return False
+
+while True:
+    
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+
+
+    # Iterate over notified sockets
+    for notified_socket in read_sockets:
+
+        # If notified socket is a server socket - new connection, accept it
+        if notified_socket == server_socket:
+
+            # Accept new connection
+            # That gives us new socket - client socket, connected to this given client only, it's unique for that client
+            # The other returned object is ip/port set
+            client_socket, client_socket1, client_socket2, client_address = server_socket.accept()
+
+            # Client should send his name right away, receive it
+            user = receive_message(client_socket)
+            username = receive_message(client_socket1)
+            userpassword = receive_message(client_socket2)
+
+            # If False - client disconnected before he sent his name
+            if user is False:
+                continue
+
+            # Add accepted socket to select.select() list
+            sockets_list.append(client_socket)
+
+            # Also save username and username header
+            clients[client_socket] = user
+            usern[client_socket1] = username
+            userp[client_socket2] = userpassword
+            if userdic.verify(usern['data'].decode('utf-8'),userp['data'].decode('utf-8')):
+                print('Accepted new connection from {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
+            else:
+                False
+
+        # Else existing socket is sending a message
+        else:
+
+            # Receive message
+            message = receive_message(notified_socket)
+
+            # If False, client disconnected, cleanup
+            if message is False:
+                print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
+
+                # Remove from list for socket.socket()
+                sockets_list.remove(notified_socket)
+
+                # Remove from our list of users
+                del clients[notified_socket]
+
+                continue
+
+            # Get user by notified socket, so we will know who sent the message
+            user = clients[notified_socket]
+
+            print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+
+            # Iterate over connected clients and broadcast message
+            for client_socket in clients:
+
+                # But don't sent it to sender
+                if client_socket != notified_socket:
+
+                    # Send user and message (both with their headers)
+                    # We are reusing here message header sent by sender, and saved username header send by user when he connected
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+    # It's not really necessary to have this, but will handle some socket exceptions just in case
+    for notified_socket in exception_sockets:
+
+        # Remove from list for socket.socket()
+        sockets_list.remove(notified_socket)
+
+        # Remove from our list of users
+        del clients[notified_socket]
