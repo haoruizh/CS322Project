29991dc9f91d40728fd3a28a2356addd87a9cfbf
MSJPurF <44460320+dgmpurf@users.noreply.github.com>
2020-05-08 21:07:45 -0700

    Update client.py

--- chatProject/client/client.py
+++ chatProject/client/client.py
@@ -12,98 +12,111 @@ choose = input("login or register: ")
 my_username = input("Username: ")
 my_password = input("Password: ")
 if choose == "register":
-    userdic.add_user(my_username, my_password)
+    #userdic.add_user(my_username, my_password)
+    file = open("userfckingname.txt","w")
+    file.writelines(my_username+' '+ my_password+'\n')
+    file.close()
+    #print(userdic.all_name)
 elif choose == "login":
-    userdic.verify(my_username, my_password)
-
-    # Create a socket
-    # socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
-    # socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-    # Connect to a given ip and port
-    client_socket.connect((IP, PORT))
-
-
-    # Set connection to non-blocking state, so .recv() call won;t block, just return some exception we'll handle
-    client_socket.setblocking(False)
+    #userdic.verify(my_username,my_password)
+    userfile = open("userfckingname.txt","r")
+    for i in userfile.readlines():
+        userfkingname = i.split()[0]
+        userfkingpassword = i.split()[1]
+        userdic.add_user(userfkingname,userfkingpassword)
+    success = userdic.verify(my_username, my_password)
+    if success is True:
+        # Create a socket
+        # socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
+        # socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-    username = my_username.encode('utf-8')
-    username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
-
-    client_socket.send(username_header + username)
+        # Connect to a given ip and port
+        client_socket.connect((IP, PORT))
 
 
-    #userdic = UserIdentify_dic()
+        # Set connection to non-blocking state, so .recv() call won;t block, just return some exception we'll handle
+        client_socket.setblocking(False)
 
-    #if userdic.verify(my_username, my_password):
+
+        username = my_username.encode('utf-8')
+        username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+
+        client_socket.send(username_header + username)
 
 
-    while True:
+        #userdic = UserIdentify_dic()
 
-        # Wait for user to input a message
-        message = input(f'{my_username} > ')
+        #if userdic.verify(my_username, my_password):
 
-        # If message is not empty - send it
-        if message:
-            # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
-            message = message.encode('utf-8')
-            message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-            client_socket.send(message_header + message)
-            #add friend firendlist could be list, waiting team done this part
-            if message.decode('utf-8') == "addfriend":
-                addfriend(message.decode('utf-8'))
-            elif message.decode('utf-8') == "showmefriendlist":
-                friendlist()
-            elif message.decode('utf-8') == "showmypasword"
-                #it depends
-                with open('userlist.txt') as read_file:
-                for line in read_file:  
-                    line = line.strip() 
-                    if line == my_username:
-                        password = next(read_file)
-                        break
-        try:
-            # Now we want to loop over received messages (there might be more than one) and print them
-            while True:
 
-                # Receive our "header" containing username length, it's size is defined and constant
-                username_header = client_socket.recv(HEADER_LENGTH)
+        while True:
 
-                # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
-                if not len(username_header):
-                    print('Connection closed by the server')
+            # Wait for user to input a message
+            message = input(f'{my_username} > ')
+
+            # If message is not empty - send it
+            if message:
+                # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
+                message = message.encode('utf-8')
+                message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+                client_socket.send(message_header + message)
+                #add friend firendlist could be list, waiting team done this part
+                """
+                if message.decode('utf-8') == "addfriend":
+                    addfriend(message.decode('utf-8'))
+                elif message.decode('utf-8') == "showmefriendlist":
+                    friendlist()
+                elif message.decode('utf-8') == "showmypasword":
+
+                    #it depends
+                    with open('userlist.txt') as read_file:
+                    for line in read_file:  
+                        line = line.strip() 
+                        if line == my_username:
+                            password = next(read_file)
+                            break
+                """
+            try:
+                # Now we want to loop over received messages (there might be more than one) and print them
+                while True:
+
+                    # Receive our "header" containing username length, it's size is defined and constant
+                    username_header = client_socket.recv(HEADER_LENGTH)
+
+                    # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+                    if not len(username_header):
+                        print('Connection closed by the server')
+                        sys.exit()
+
+                    # Convert header to int value
+                    username_length = int(username_header.decode('utf-8').strip())
+
+                    # Receive and decode username
+                    username = client_socket.recv(username_length).decode('utf-8')
+
+                    # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
+                    message_header = client_socket.recv(HEADER_LENGTH)
+                    message_length = int(message_header.decode('utf-8').strip())
+                    message = client_socket.recv(message_length).decode('utf-8')
+
+                    # Print message
+                    print(f'{username} > {message}')
+
+
+
+            except IOError as e:
+
+                if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                    print('Reading error: {}'.format(str(e)))
                     sys.exit()
 
-                # Convert header to int value
-                username_length = int(username_header.decode('utf-8').strip())
+                continue
 
-                # Receive and decode username
-                username = client_socket.recv(username_length).decode('utf-8')
-
-                # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
-                message_header = client_socket.recv(HEADER_LENGTH)
-                message_length = int(message_header.decode('utf-8').strip())
-                message = client_socket.recv(message_length).decode('utf-8')
-
-                # Print message
-                print(f'{username} > {message}')
-
-
-
-        except IOError as e:
-
-            if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-                print('Reading error: {}'.format(str(e)))
+            except Exception as e:
+                print('Reading error: '.format(str(e)))
                 sys.exit()
 
-            continue
-
-        except Exception as e:
-            print('Reading error: '.format(str(e)))
-            sys.exit()
-
-    #    else:
-    #        False
+        #    else:
+        #        False
