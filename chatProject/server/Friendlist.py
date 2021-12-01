fc983d11130dcf13c347aa663ccba1d0fdd62945
jsheng <jsheng@eecs.wsu.edu>
2021-12-01 20:36:58 +0800

    update

--- chatProject/server/Friendlist.py
+++ chatProject/server/Friendlist.py
@@ -1,25 +1,35 @@
 from socket import *
 import json
 class Friendlist:
+    ContactList = []
     def __init__(self):
-        self.ContactList = []
-    def View(self):
+        pass
+    def ViewFriendList(self):
         for i in self.ContactList:
             print(i)
-    def Add(self, Name):
+    def AddFriend(self, Name):
         #if searched user not in the list
         if Name not in self.ContactList:
             #If try to add own name, throw an error
             self.ContactList.append(Name)
         else:
             print("Searched user already in the list!")
-    def Remove(self, Name):
+    def RemoveFriend(self, Name):
         if Name in self.ContactList:
             self.ContactList.remove(Name)
         else:
             print("Searched user not in the list!")
-    def Find(self, Name):
-        if Name in self.ContactList:
-            return self.ContactList[Name]
-        else:
-            print("Searched user not in list!")
+    def FindUser(self, Name):
+        for i in range(len(self.ContactList)):
+            if self.ContactList[i] == Name:
+                return self.ContactList[i]
+            else:
+                print('User not in list')
+
+    def test_f(self):
+        Friendlist().AddFriend('na')
+        Friendlist().FindUser('na')
+        print(Friendlist().ContactList)
+
+if __name__ == "__main__":
+    Friendlist().test_f()
